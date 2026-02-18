ac7275ce1c93305f9024355278162e404b9b4dc8
Gaser Moussa <76227165+Gaser98@users.noreply.github.com>
2026-02-19 01:23:37 +0200

    Enhance comments in allow-specific-traffic.sh
    Added comments to clarify the importance of rule order and persistence.

--- allow-specific-traffic.sh
+++ allow-specific-traffic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 sudo yum install -y iptables iptables-services
-sudo iptables -I INPUT -p tcp --dport <Port> -s <IP> -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport <Port> -s <IP> -j ACCEPT #order matters
 sudo iptables -A INPUT -p tcp --dport <Port> -j DROP
-sudo service iptables save
+sudo service iptables save  #makes the rules persistent even after reboot
 sudo iptables -L INPUT -v -n --line-numbers
